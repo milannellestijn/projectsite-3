18c33f583c8f9d4f435bf3f56387fe1f44f5dfb1
Milan Nellestijn <323101@student.mboutrecht.nl>
2019-03-13 10:11:57 +0100

    Update V1.7

---
+++ container.php
@@ -0,0 +1,24 @@
+<div class="container marketing">
+
+<!-- Three columns of text below the carousel -->
+<div class="row">
+  <div class="col-lg-4">
+     <img src="img/game.png" width="155" height="145">
+    <h2>Gratis zelftest</h2>
+    <p>De geheugentraining is een gratis zelftest die laten zien hoe goed jij bent in het onthouden van dingen.</p>
+    <p><a class="btn btn-secondary" href="#" role="button">Doe de test »</a></p>
+  </div><!-- /.col-lg-4 -->
+  <div class="col-lg-4">
+      <img src="img/more.png" width="150" height="150">
+    <h2>Over</h2>
+    <p>Deze website is bedoeld om mensen informatie te geven over het geheugen en hoe dit in elkaar zit. </p>
+    <p><a class="btn btn-secondary" href="#" role="button">Meer lezen »</a></p>
+  </div><!-- /.col-lg-4 -->
+  <div class="col-lg-4">
+     <img src="img/contact.png" width="135" height="135">
+    <h2>Contact</h2>
+    <p>Heeft U niet alles kunnen vinden waar u naar zocht, neem dan contact met ons op voor meer informatie!</p>
+    <p><a class="btn btn-secondary" href="#" role="button">Contact opnemen »</a></p>
+  </div><!-- /.col-lg-4 -->
+</div><!-- /.row -->
+</div><!-- /.container -->
--- index.php
+++ index.php
@@ -23,37 +23,14 @@
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
 
 <div class="container-fluid">
+
+<!-- include de carousel in de pagina -->
 <?php include('carousel.php'); ?>
 
+<!-- include de container met info in de pagina -->
+<?php include('container.php'); ?>
 
-  <!-- Marketing messaging and featurettes
-  ================================================== -->
-  <!-- Wrap the rest of the page in another container to center all the content. -->
 
-  <div class="container marketing">
-
-    <!-- Three columns of text below the carousel -->
-    <div class="row">
-      <div class="col-lg-4">
-         <img src="img/game.png" width="155" height="145">
-        <h2>Gratis zelftest</h2>
-        <p>De geheugentraining is een gratis zelftest die laten zien hoe goed jij bent in het onthouden van dingen.</p>
-        <p><a class="btn btn-secondary" href="#" role="button">Doe de test »</a></p>
-      </div><!-- /.col-lg-4 -->
-      <div class="col-lg-4">
-          <img src="img/more.png" width="150" height="150">
-        <h2>Over</h2>
-        <p>Deze website is bedoeld om mensen informatie te geven over het geheugen en hoe dit in elkaar zit. </p>
-        <p><a class="btn btn-secondary" href="#" role="button">Meer lezen »</a></p>
-      </div><!-- /.col-lg-4 -->
-      <div class="col-lg-4">
-         <img src="img/contact.png" width="135" height="135">
-        <h2>Contact</h2>
-        <p>Heeft U niet alles kunnen vinden waar u naar zocht, neem dan contact met ons op voor meer informatie!</p>
-        <p><a class="btn btn-secondary" href="#" role="button">Contact opnemen »</a></p>
-      </div><!-- /.col-lg-4 -->
-    </div><!-- /.row -->
-  </div><!-- /.container -->
 </main>
 
    <!-- Include de footer in de pagina -->
