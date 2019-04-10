0ccd4d0fe80c71d08e1667765af45061395ce299
dolle3 <43133285+dolle3@users.noreply.github.com>
2019-04-10 09:31:31 +0200

    a
    push

--- game.php
+++ game.php
@@ -4,24 +4,40 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
     <link href="./css/dashboard.css" rel="stylesheet">
     <link href="./css/style.css" rel="stylesheet">
-    <link href="./css/carousel.css" rel="stylesheet">
     <link href="/docs/4.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-
+    <link rel="shortcut icon" href="./GameBuild/TemplateData/favicon.ico">
+    <link rel="stylesheet" href="./GameBuild/TemplateData/style.css">
+    <script src="./GameBuild/TemplateData/UnityProgress.js"></script>
+    <script src="./GameBuild/Build/UnityLoader.js"></script>
+    <script>
+      var gameInstance = UnityLoader.instantiate("gameContainer", "./GameBuild/Build/Nieuwe map.json", {onProgress: UnityProgress});
+    </script>
     <title>Home</title>
   </head>
   <body>
     <!-- include de navigatie in de pagina -->
   <?php include('navbar.php'); ?>
 
-   <!-- content -->
-<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+<div class="game">
 
+  <!-- content -->
+<main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+  <div class="webgl-content">
+    <div id="gameContainer" style="width: 960px; height: 600px"></div>
+    <div class="footer">
+      <div class="webgl-logo"></div>
+      <div class="fullscreen" onclick="gameInstance.SetFullscreen(1)"></div>
+      <div class="title">ProjectPer3</div>
+    </div>
+  </div>
+</div>
 <div class="container-fluid">
 
 </main>
